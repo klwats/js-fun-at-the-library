0f2478603d818c41fed69d2f95d19728b9a01dd2
Kelli Watkins <watkinskellil@gmail.com>
2022-11-30 18:08:35 -0700

    Add file

--- src/warm-up.js
+++ src/warm-up.js
@@ -13,26 +13,27 @@
 // var title = "Harry Potter";
 // var ...
 
-
+var title = "Wait, What?"
+var pages = 1307
 
 // Reassign (not initialize) your "pages" variable from above. An editor came around and
 // condensed the book, use a built in math operator to make the page count a quarter
 // of what it currently is 
-
+pages = Math.round(pages * 0.25)
 
 
 // Initialize a new variable "movie", but leave it undefined
-
+var movie;
 
 
 // Create a variable named "sequel", and assign it to a be a sequel of your
 // book (made up or not!). Your sequel must make use of your "title" variable from above
-
+var sequel = `${title}: Still Waiting`
 
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the book you chose is your favorite book or not
-
+var isFavorite = true
 
 
 
@@ -40,6 +41,8 @@
 // assign a boolean value to this variable. If "pages" is less than 25, the
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
+
+var isShortStory = pages < 25
 
 
 
@@ -50,21 +53,29 @@
 // the statement 'I could read that in a day!', otherwise, log the statement 
 // 'Ok, maybe I could read that in a day and a half'
 
-
+if (pages < 1000) {
+    console.log(`I could read that in a day!`)
+} else {
+    console.log(`Ok, maybe I could read that in a day and a half`)
+}
 
 // Express the following in code: If the variable "isFavorite" is true,
 // log the statement 'My favorite book is [title]!'. Otherwise, log the
 // statement 'I recently read the [title] book'. In either case, the value stored in
 // the "title" variable should be included in the logged statement.
 
-
+if (isFavorite === true) {
+    console.log(`My favorite book is ${title}`)
+} else {
+    console.log(`I recently read the ${title} book`)
+}
 
 //------------------------- Arrays -------------------------//
 
 
 // Create a new variable "books" and assign it to an Array of 5 different
 // book titles, represented by their names as Strings.
-
+var books = ["Of Mice and Men", "Ulysses", "Don Quixote", "The Great Gatsby", "Moby Dick"]
 
 // Write the code to access the 3rd element of the Array, using bracket
 // notation.
