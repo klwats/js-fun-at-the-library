bf0a0fbdf703fa65920c68042fad4655dc9524b9
Kelli Watkins <watkinskellil@gmail.com>
2022-12-01 20:57:44 -0700

    Add completed books

--- src/book.js
+++ src/book.js
@@ -1,12 +1,44 @@
-function createTitle() {
+function createTitle(title) {
+  return `The ${title}`
+}
+function buildMainCharacter(name, age, pronouns) {
+    var character = 
+    {
+      name: name,
+      age: age,
+      pronouns: pronouns
+    }
+    return character
+}
+function saveReview(review, reviewsCollection) {
   
+  if (reviewsCollection.includes(review) === false) {
+    reviewsCollection.push(review)
+  }
+}
+
+function calculatePageCount(title) {
+return title.length * 20
+}
+function writeBook(title, character, genre) {
+ var book = {
+  title: title,
+  mainCharacter: character,
+  pageCount: calculatePageCount(title),
+  genre: genre
+ }
+ return book
+}
+function editBook(book) {
+book.pageCount = book.pageCount * 0.75
+return book
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
