86f2078f36558ca0b41aab3efbe78253db351760
Kelli Watkins <watkinskellil@gmail.com>
2022-12-03 16:46:44 -0700

    Add list titles function

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,38 @@
+function shelfBook(book, shelfName) {
+   if (shelfName.length < 3) {
+    return shelfName.unshift(book)
+   } 
+}
+  
+  function unshelfBook(book, shelfName) {
+    for (var i = 0; i < shelfName.length; i++) {
+      if (book === shelfName[i].title) {
+    return shelfName.splice(i, 1)
+  }
+    }
+  }
 
+  function listTitles(shelfName) {
+    var titles = []
+    for (var i = 0; i < shelfName.length; i++) {
+      titles.push(shelfName[i].title)
+    }
+    return titles.join(', ')
+  }
+
+  function searchShelf() {
+
+  }
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
+
+
+
+
+
+
+
