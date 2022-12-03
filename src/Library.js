ab549f48081855578b4e5afedf9034597ebd55f1
Kelli Watkins <watkinskellil@gmail.com>
2022-12-02 21:34:09 -0700

    Add library function

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,20 @@
 
 
+function createLibrary(name) {
+  var library = {
+    name: name, 
+    shelves: {
+      fiction: [],
+      fantasy: [],
+      nonFiction: []
+    }
+    
+  }
+  return library
+}
+
 module.exports = {
-  // createLibrary,
+  createLibrary,
   // addBook,
   // checkoutBook
 };
