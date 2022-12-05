dfe1e6a6f752dfe13fec90f06c119b8f667650bc
Kelli Watkins <watkinskellil@gmail.com>
2022-12-04 19:24:09 -0700

    "Add addbook and checkoutbook"

--- src/Library.js
+++ src/Library.js
@@ -13,8 +13,22 @@ function createLibrary(name) {
   return library
 }
 
+function addBook(library, book) {
+  library.shelves[book.genre].push(book)
+}
+
+function checkoutBook(library, title, genre) {
+  for (var i = 0; i < library.shelves[genre].length; i++) {
+    if (library.shelves[genre][i].title === title){
+      library.shelves[genre].splice(i)
+      return `You have now checked out ${title} from the ${library.name}`
+    }
+  }  
+  return `Sorry, there are currently no copies of ${title} available at the ${library.name}`
+}
 module.exports = {
   createLibrary,
-  // addBook,
-  // checkoutBook
+  addBook,
+  checkoutBook
 };
+
